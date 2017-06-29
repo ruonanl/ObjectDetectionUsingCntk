15a645a63ceb148cba5641ca3d00d46a2a77c154
DRONEIMAGECNTK2\ruonanli <ruonanl@microsoft.com>
2017-06-29 15:38:10 +0000

    temporarily remove past_div

--- helpers_cntk.py
+++ helpers_cntk.py
@@ -1,6 +1,6 @@
 from __future__ import division
 from __future__ import print_function
-from past.utils import old_div
+#from past.utils import old_div
 
 import os, pdb, sys, numpy as np
 from os.path import join
